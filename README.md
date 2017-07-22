82fa62e05dffb8303886d3c173a023c50ef1fdde
Lucas Cardozo <lucasecardozo@gmail.com>
2017-07-22 15:59:00 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# rworker
+Remote R functions execution mediated by Redis.
+## Usage
+```
+rworker your_functions.R --queue='your-queue-name' --redis-addr='your-redis-ip-addr' --workers=3
+```
---
+++ examples/tasks.R
@@ -0,0 +1,13 @@
+task1 <- function(){
+    for (i in 1:10) {
+        print(i)
+        Sys.sleep(2)
+    }
+}
+
+task2 <- function(){
+    for (i in 1:20) {
+        print(i)
+        Sys.sleep(1)
+    }
+}
