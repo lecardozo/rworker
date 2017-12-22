1dbd0d56960e395a2af0e3865ef06b3f41ae4841
Lucas Cardozo <lucasecardozo@gmail.com>
2017-12-22 03:06:22 -0200

    preparing for future implementation of different message passing providers

---
+++ R/redis_queue.R
@@ -0,0 +1,56 @@
+#' @importFrom R6 R6Class
+#' @import redux
+NULL
+
+#' RedisQueue object
+#'
+#' This object establishes an interface for Redis as a message broker
+#'
+#' @section Usage:
+#' ```
+#' queue <- RedisQueue$new(host='localhost',
+#'                         port=6379, name='celery')
+#' msg <- queue$pull()
+#' queue$push(msg)
+#' ```
+#' @param name The name of the queue.
+#' @param host Message broker instance address.
+#' @param port Message broker port.
+#'
+#' @name RedisQueue
+NULL
+
+#' @export 
+RedisQueue <- R6::R6Class(
+    'RedisQueue',
+    public = list(
+        host = NULL,
+        port = NULL,
+        name = NULL,
+
+        initialize = function(name='celery', host='localhost', port=6379) {
+            self$host = host
+            self$port = port
+            if(missing(name)) {
+                   stop('Must provide Queue name')
+            } else {
+                self$name = name
+            }
+            private$channel = redux::hiredis(host=self$host,
+                                             port=self$port)
+        },
+
+        pull = function() {
+            msg = private$channel$LPOP(self$name)
+            return(msg)
+        },
+
+        push = function(msg) {
+            private$channel$LPUSH(self$name, msg)
+        }
+    ),
+
+    private = list(
+        channel = NULL
+    )
+)
